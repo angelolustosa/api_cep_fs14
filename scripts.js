9ba8f2a693eb809881c042d21b8ca7f78cb35895
Angelo <angelolustosa@gmail.com>
2023-06-01 22:09:12 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const findEstados = () => {
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then(response => response.json())
+        .then(json => {
+            //console.log(json)
+            let inputEstado = document.getElementById('estado')
+
+            let estados = ''
+            json.forEach(estado =>
+                estados += `<option value=${estado.sigla}>${estado.nome}</option>`
+            );
+            console.log(estados)
+            inputEstado.innerHTML = estados
+            
+        })
+}
+
+findEstados()
